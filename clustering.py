64bf0ab11a252f78c9bff616fbd7a587408f13cd
Avery <avery.mavroudis@epifany.com>
2018-06-05 13:58:21 -0400

    Removed subsetting practice

--- clustering.py
+++ clustering.py
@@ -11,17 +11,6 @@ from sklearn.cluster import DBSCAN
 
 locations = pd.read_csv('./locations.csv')
 
-
-#Subsetting with a single user
-user53535 = locations.loc[(locations.id == 53535),('latitude','longitude')]
-
-#DBSCAN with user 53535
-db = DBSCAN(eps = 0.1, min_samples = 5).fit(user53535)
-user53535['clusters'] = db.labels_
-clusters = user53535.groupby('clusters').agg([np.mean, np.size])
-clusters.columns = ['latitude', 'size0', 'longitude', 'size']
-clusters.drop(columns = 'size0')
-
 def cluster(locations):
     coordinates = locations[['latitude','longitude']]
     db = DBSCAN(eps = 0.1, min_samples = 5).fit(coordinates)
