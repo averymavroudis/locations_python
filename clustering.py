50c06bc49fe7c533a0d654573bf43b9191b41e9c
Avery <avery.mavroudis@epifany.com>
2018-06-05 15:53:17 -0400

    Made script available through terminal

--- clustering.py
+++ clustering.py
@@ -5,11 +5,10 @@ Created on Tue May 22 17:27:36 2018
 
 @author: AveryMavroudis
 """
+import sys
 import pandas as pd
 import numpy as np
 from sklearn.cluster import DBSCAN
-
-locations = pd.read_csv('./locations.csv')
 
 def cluster(locations):
     coordinates = locations[['latitude','longitude']]
@@ -28,5 +27,12 @@ def locations_csv(data, file_name = ""):
     max_cluster = outliers_rm.groupby('id').apply(max)
     locations = max_cluster.drop(columns = 'cluster')
     locations.to_csv(file_name, index = False)
+
+if len(sys.argv) == 3:
+    locations = pd.read_csv(sys.argv[1])
+    locations_csv(locations, sys.argv[2])
+else:
+    print("usage: python clustering.py input.csv output.csv ")   
+    print("For more information, please visit https://github.com/averymavroudis/locations_python")
     
     
